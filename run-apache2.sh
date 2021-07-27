56cbc0d530a14f0b73f0f8ffada9dd23987e00a5
Mariohs22 <serge.burakovsky@gmail.com>
2021-07-27 15:43:03 +0300

    update starting script

--- run-apache2.sh
+++ run-apache2.sh
@@ -2,5 +2,6 @@
 
 # sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 # apache2-foreground "$@"
+a2dismod mpm_event
 sed -i "s/Listen 80/Listen ${PORT:-80}/g" /etc/apache2/ports.conf
 /usr/local/bin/docker-php-entrypoint apache2-foreground
