0a5c38126111cbb8e0bac417577fb1f4fec776df
Mariohs22 <serge.burakovsky@gmail.com>
2021-07-27 23:10:22 +0300

    add telegram notify

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # app1_php
+
+LOGS:
+`heloku log -a APP_NAME --tail`
--- app/ClassExample.php
+++ app/ClassExample.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App;
 require 'Greeter.php';
-//use App\Greeter;
 
 $greeter = new Greeter();
 echo $greeter->greet('World');
